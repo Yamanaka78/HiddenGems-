11d895b79aad8253859efb86db81e5f27540ecfb
Yamanaka78 <jjbdumujpgm@gmail.com>
2024-11-01 22:50:59 +0900

    詳細画面のコメント一覧表示の作成

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -21,6 +21,8 @@ class SpotsController < ApplicationController
 
   def show
     @spot = Spot.find(params[:id])
+    @review = Review.new
+    @reviews = @spot.reviews.includes(:user).order(created_at: :desc).limit(2)
   end
 
 
