6bb28aefd9023ca38f3658747483044d126192a2
Yamanaka78 <jjbdumujpgm@gmail.com>
2024-11-13 22:25:30 +0900

    homeにplan一覧を表示処理追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: %i[index]
   def index
     @spots = Spot.includes(:user, :tags).limit(10)
+    @plans = Plan.where(public: true).includes(:user, :spots).order(created_at: :desc).limit(10)
   end
 end
