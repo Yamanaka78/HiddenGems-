bcaad673bf0319993313ff4b399d574b2d759013
Yamanaka78 <jjbdumujpgm@gmail.com>
2024-12-03 22:04:37 +0900

    お気に入り機能を非同期処理へ変更

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,13 +1,11 @@
 class WishlistsController < ApplicationController
   def create
-    spot = Spot.find(params[:spot_id])
-    current_user.wishlist(spot)
-    redirect_to spots_path, success: t(".success")
+    @spot = Spot.find(params[:spot_id])
+    current_user.wishlist(@spot)
   end
 
   def destroy
-    spot = current_user.wishlists.find(params[:id]).spot
-    current_user.unwishlist(spot)
-    redirect_to spots_path, success: t(".success"), status: :see_other
+    @spot = current_user.wishlists.find(params[:id]).spot
+    current_user.unwishlist(@spot)
   end
 end
